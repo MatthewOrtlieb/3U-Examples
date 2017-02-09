50bf3ae0f73ab4480803a33cda06b38347803660
ortlm8239 <ortlm8239@HRH-IC0027325.wrdsb.ca>
2017-02-09 10:00:47 -0500

    initial commit, first example added

---
+++ src/Example1.java
@@ -0,0 +1,31 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ortlm8239
+ */
+public class Example1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // a new city called 
+        City kitchener = new City();
+        
+        //creating a robot
+        Robot karel = new Robot(kitchener,2,1,Direction.EAST);
+        karel.move();
+        karel.turnLeft();
+    }
+  
+}
